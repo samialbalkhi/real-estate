a68eec4c98cd0b7cf759b26832e237ea7ef5dea3
samialbalkhi <samialbalkhi@users.noreply.github.com>
2023-12-20 01:09:34 +0000

    Dusting

--- database/migrations/2023_12_19_005051_create_reviews_table.php
+++ database/migrations/2023_12_19_005051_create_reviews_table.php
@@ -1,38 +1,32 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->unsignedTinyInteger('rating')->default(1);
-            $table->foreignIdFor(User::class)  /////// The person who made the evaluation  
+            $table->foreignIdFor(User::class)  /////// The person who made the evaluation
                 ->constrained()
                 ->cascadeOnDelete()
                 ->cascadeOnUpdate();
 
-                $table->unsignedBigInteger('rated_user_id');   //// The person who was evaluated
-                $table->foreign('rated_user_id')
-                    ->references('id')
-                    ->on('users')
-                    ->onDelete('cascade')
-                    ->onUpdate('cascade');
+            $table->unsignedBigInteger('rated_user_id');   //// The person who was evaluated
+            $table->foreign('rated_user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('reviews');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,20 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\User;
-use App\Models\Offer;
-use App\Models\Order;
-use App\Models\Report;
-use App\Models\Review;
-use App\Models\Product;
-use App\Models\Section;
-use App\Models\LoanTerm;
 use App\Models\AccountType;
 use App\Models\Advertisement;
-use App\Models\RealEstateType;
 use App\Models\HomePageContent;
-use Illuminate\Database\Seeder;
+use App\Models\LoanTerm;
+use App\Models\Offer;
+use App\Models\Order;
+use App\Models\Product;
 use App\Models\RealEstateCategory;
+use App\Models\RealEstateType;
+use App\Models\Report;
+use App\Models\Review;
+use App\Models\Section;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
