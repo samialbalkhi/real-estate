d2fd5749b5df48294e2ed350bfe0f5f0973a3593
samialbalkhi <samialbalkhi766@gmail.com>
2024-01-10 23:50:12 +0100

    Register completed agora

---
+++ app/Jobs/RegistrationAgora.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Jobs;
+
+use GuzzleHttp\Client;
+use Illuminate\Bus\Queueable;
+use PHPUnit\Event\Code\Throwable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\Log;
+
+class RegistrationAgora implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    protected $user;
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $agoraAppId = '7b99b46b4fbb4fc183a73d0c4ed33054';
+        $agoraAppCertificate = 'e5018477219141c3ad7bd21c493a863c';
+        $agoraRsetApi = 'a71.chat.agora.io';
+        $agoraOrgName = '711084304';
+        $agoraAppName = '1264508';
+        $agoraApiUrl = "https://{$agoraRsetApi}/{$agoraOrgName}/{$agoraAppName}/users";
+
+        $client = new Client();
+
+        $response = $client->post($agoraApiUrl, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . '007eJxTYEg4PFNeyLxN5HLnyhaNzey9IXNXfnj1lqnipsZeyUVPxDoVGMyTLC2TTMySTNKSgDjZ0MI40dw4xSDZJDXF2NjA1MRn57zUhkBGhuXRbKyMDKwMjAxMDCA+AwMAh/cdfg==',
+            ],
+            'json' => [
+                'username' => (string) $this->user->id,
+                'app_id' => $agoraAppId,
+                'app_certificate' => $agoraAppCertificate,
+                'user_id' => $this->user->id,
+            ],
+        ]);
+
+        $token = $this->user->createToken('token-name', ['user'])->plainTextToken;
+        $agora_data = ['agora_data' => json_decode($response->getBody(), true)];
+
+    }
+    public function failed(Throwable $e)
+    {
+        info('Something went wrong');
+    }
+}
--- app/Service/Frontend/RegisterService.php
+++ app/Service/Frontend/RegisterService.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use GuzzleHttp\Client;
 use App\Traits\ImageUpload;
 use App\helpers\ApiResponse;
+use App\Jobs\RegistrationAgora;
 use App\Http\Requests\Frontend\RegisterRequest;
 
 class RegisterService
@@ -14,37 +15,13 @@ class RegisterService
 
     public function store(RegisterRequest $request)
     {
-      $user = User::create([
-        'image' => $this->uploadImage('user_image'),
-    ] + $request->validated());
+        $user = User::create(
+            [
+                'image' => $this->uploadImage('user_image'),
+            ] + $request->validated(),
+        );
 
-    $agoraAppId = '611081744#1261637';
-    // $agoraAppCertificate = 'YOUR_AGORA_APP_CERTIFICATE';
-    $agoraApiUrl =  'https://XXXXXXXXXXXXX/611081744/1261637/'.$user;
-    ;
-
-    $client = new Client();
-    try {
-        $response = $client->post($agoraApiUrl, [
-            'json' => [
-                'app_id' => $agoraAppId,
-                // 'app_certificate' => $agoraAppCertificate,
-                'user_id' => $user->id, // Assuming user ID is unique and can be used as Agora user ID
-                // Add any other necessary user data for Agora Chat
-            ],
-        ]);
-
-        $responseData = json_decode($response->getBody(), true);
-
-        // Handle the Agora Chat response data as needed
-        // You might store Agora user ID or other relevant information in your local database
-
-        $token = $user->createToken('token-name', ['user'])->plainTextToken;
-
-        return ['token' => $token, 'agora_data' => $responseData];
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 500);
-    }
-        // return ApiResponse::createSuccessResponse();
+        RegistrationAgora::dispatch($user);
+        return ApiResponse::createSuccessResponse();
     }
 }
